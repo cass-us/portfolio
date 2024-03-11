af20cdfe0d6b5cf725b4474abf6cff25b69b558b
Cassius Makalapetlo <cassiusmaropene@gmail.com>
2024-03-11 03:08:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleManu(){
+const manu = document.querySelector(".manu-link");
+const icon = document.querySelector(".ham-icon");
+manu.classList.toggle("open");
+icon.classList.toggle("open");
+
+}
